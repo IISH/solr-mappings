4cb3d7062bd4751acc4d30110003d196ea7795b3
Lucien van Wouw <lwo@iisg.nl>
2013-02-13 19:29:50 +0100

    Get latest build

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget --no-check-certificate -O /usr/bin/vufind/import-1.0.jar https://bamboo.socialhistoryservices.org/browse/SEARCH-SEARCH-155/artifact/JOB1/import-1.0.jar/import-1.0.jar
+b=https://bamboo.socialhistoryservices.org/browse/SEARCH-SEARCH/latestSuccessful/artifact/JOB1/import-1.0.jar/import-1.0.jar
+wget --no-check-certificate -O /usr/bin/vufind/import-1.0.jar $b
